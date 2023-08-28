dc55390a1f4198865b0d71c7a4d4bed9d8ebdfc8
Korchea <6334@holbertonstudents.com>
2023-08-28 09:10:03 -0700

    Task 7 completed

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-""" This type-annotated function sum_mixed_list which takes a list mxd_lst
-of integers and floats and returns their sum as a float."""
-from typing import List, Union
+""" This type-annotated function to_kv that takes a string k and an int OR
+float v as arguments and returns a tuple."""
+from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple:
-    """ This type-annotated function sum_mixed_list which takes a list mxd_lst
-    of integers and floats and returns their sum as a float."""
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """ This type-annotated function to_kv that takes a string k and an int
+    OR float v as arguments and returns a tuple."""
     return (k, v**2)
