9ef8e06580c914115c0fb7e015b9075f37dc7edf
Korchea <6334@holbertonstudents.com>
2023-08-29 11:48:24 -0700

    Task 11, advancedm completed

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -6,7 +6,7 @@ T = TypeVar('T')
 
 
 def safely_get_value(dct: Mapping, key: Any,
-                     default: Union[~T, None] = None) -> Union[Any, ~T]:
+                     default: Union[T, None] = None) -> Union[Any, T]:
     """ Add function’s parameters and return values with the
     appropriate types"""
     if key in dct:
