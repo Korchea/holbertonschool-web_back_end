5d4a8cc8167179e7a5be61c18a376e959161a5c8
Korchea <6334@holbertonstudents.com>
2023-08-29 11:33:25 -0700

    Task 10, advancedm completed

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -4,7 +4,8 @@ from typing import Sequence, Any, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    """ Add function’s parameters and return values with the appropriate types"""
+    """ Add function’s parameters and return values with the
+    appropriate types"""
     if lst:
         return lst[0]
     else:
