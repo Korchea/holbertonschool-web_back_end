34e2bb70f0c93e757f1165bc0ac4de7eb0dd047b
Korchea <6334@holbertonstudents.com>
2023-08-29 11:32:19 -0700

    Task 10, advancedm completed

---
+++ python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Add function’s parameters and return values with the appropriate types"""
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """ Add function’s parameters and return values with the appropriate types"""
+    if lst:
+        return lst[0]
+    else:
+        return None
