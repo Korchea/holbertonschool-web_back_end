517089f8ba628a71e30b17e767b0ef48ea88ccc6
Korchea <6334@holbertonstudents.com>
2023-09-05 14:17:36 -0700

    Task 8 completed

---
+++ NoSQL/8-all.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Write a Python function that lists all documents in a collection"""
+
+
+def list_all(mongo_collection):
+    """ Write a Python function that lists all documents in a collection"""
+    if mongo_collection is not None:
+        return mongo_collection.find()
+    return []
