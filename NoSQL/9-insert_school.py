f385e852219567eb2c4491ed5861eb6c7229f2df
Korchea <6334@holbertonstudents.com>
2023-09-05 14:36:36 -0700

    Task 9 to check

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -6,9 +6,4 @@ collection based on kwargs"""
 def insert_school(mongo_collection, **kwargs):
     """ Python function that inserts a new document in a
     collection based on kwargs"""
-    in_dict = {}
-    for arg in kwargs:
-        arg.replace('=', ": ")
-        arg = '{' + arg + '}'
-        in_dict.update(arg)
-    return mongo_collection.insert_one(in_dict)
+    return mongo_collection.insert_one(kwargs).inserted_id
