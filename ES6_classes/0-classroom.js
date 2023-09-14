8e9326599ab172d52129e56841071cfb2ad0445a
Korchea <6334@holbertonstudents.com>
2023-09-14 07:24:49 -0700

    Task 0 to test

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -2,12 +2,4 @@ export default class ClassRoom {
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
-
-  get maxStudentsSize() {
-    return this._maxStudentsSize;
-  }
-
-  set maxStudentsSize(maxStudents) {
-    this._maxStudentsSize = maxStudents;
-  }
 }
