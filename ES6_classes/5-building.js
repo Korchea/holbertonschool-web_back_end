9ad1c6d821520ad4b2ee7ef77c475d035d6a4e2a
Korchea <6334@holbertonstudents.com>
2023-09-14 13:02:26 -0700

    Task 5 completed

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,18 @@
+export default class Building {
+    constructor(sqft) {
+        if (this.constructor !== Building) {
+            throw new Error('Class extending Building must override evacuationWarningMessage');
+        }
+        if (typeof sqft !== 'number') throw TypeError('Sqft must be a number');
+        this._sqft = sqft;
+    }
+
+    get sqft() {
+        return this._sqft;
+    }
+
+    set sqft(newSqft) {
+        if (typeof newSqft !== 'number') throw TypeError('Sqft must be a number');
+        this._sqft = newSqft;
+    }
+}
