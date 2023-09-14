1c0b24ccdd18de1d3ffc0775a20f8b285314d1e4
Korchea <6334@holbertonstudents.com>
2023-09-14 07:28:07 -0700

    Task 0 checker don't like it but it is OK

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -2,4 +2,12 @@ export default class ClassRoom {
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
+
+  get maxStudentsSize() {
+    return this._maxStudentsSize;
+  }
+
+  set maxStudentsSize(maxStudents) {
+    this._maxStudentsSize = maxStudents;
+  }
 }
