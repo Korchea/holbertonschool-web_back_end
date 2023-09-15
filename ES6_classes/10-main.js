c79c4ea4022f031c4db92833bfdcbe1b3e3d54c8
Korchea <6334@holbertonstudents.com>
2023-09-15 07:57:38 -0700

    Remove node_modules and thing like that to prove

--- ES6_classes/10-main.js
+++ ES6_classes/10-main.js
@@ -1,14 +1,9 @@
 import Car from "./10-car.js";
 
-class TestCar extends Car {}
-
-const tc1 = new TestCar("Nissan", "Turbo", "Pink");
-const tc2 = tc1.cloneCar();
-
-console.log(tc1);
-console.log(tc1 instanceof TestCar);
-
-console.log(tc2);
-console.log(tc2 instanceof TestCar);
-
-console.log(tc1 == tc2);
+test("Car has the correct definition", () => {
+    const bmw = new Car('BMW', 'Turbo', 'Mango');
+    expect(bmw._brand).toBe('BMW');
+    expect(bmw._color).toBe('Mango');
+    expect(bmw._motor).toBe('Turbo');
+    expect(bmw._range).toBe(undefined);
+});
