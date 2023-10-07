6276158aefec9935a7442bfa99ca7f917c55be98
Korchea <6334@holbertonstudents.com>
2023-10-07 15:53:42 -0700

    Task 3 not completed

---
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,4 @@
+export default function getStudentIdsSum(studentList) {
+    const initialValue = 0;
+    return studentList.reduce((accumulator, currentValue) => accumulator + currentValue, initialValue);
+}
