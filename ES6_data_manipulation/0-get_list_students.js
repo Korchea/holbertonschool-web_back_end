d716fc1108715d0e24c19130ea9e03a75ceafd0f
Korchea <6334@holbertonstudents.com>
2023-10-07 10:40:51 -0700

    Task 0 completed

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,18 @@
+export default function getListStudents() {
+  const student_1 = {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco'
+  };
+  const student_2 = {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia'
+  };
+  const student_3 = {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco'
+  };
+  return [student_1, student_2, student_3];
+}
