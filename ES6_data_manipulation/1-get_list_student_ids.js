154815f03b8b0acb83a352b53a16f7cf38e2a188
Korchea <6334@holbertonstudents.com>
2023-10-07 11:14:41 -0700

    Task 1 completed

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,9 +1,8 @@
 export default function getListStudentIds(studentList) {
-  let idList = [];
   if (studentList instanceof Array) {
-    for (const student of studentList) {
-      idList.push(student['id']);
-    }
+    const idList = studentList.map(student => student['id']);
+    return idList;
+  } else {
+    return [];
   }
-  return idList;
 }
