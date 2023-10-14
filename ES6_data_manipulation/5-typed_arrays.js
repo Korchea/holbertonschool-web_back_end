eedd9feed388d584f1e97db092e3484405ccafd9
Korchea <6334@holbertonstudents.com>
2023-10-14 13:13:04 -0700

    Task 5 completed

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,11 @@
 export default function createInt8TypedArray(length, position, value) {
-    if (position < length) {
-        throw Error('Position outside range');
-    }
+  if (position > length) {
+    throw Error('Position outside range');
+  }
+
+  const buffer = new ArrayBuffer(length);
+  const dataview = new DataView(buffer);
+  // 'setInt8' is used to set a signed 8-bit value at the specified position.
+  dataview.setInt8(position, value);
+  return dataview;
 }
